6bbda45301caff922b4a45797b497e1fa3ccb7d2
greenmoon55 <greenmoon55@gmail.com>
2012-06-14 00:23:34 +0800

    小改添加新书的界面，BackTerminal还差大量代码。。

--- BackTerminal/frmBookNew.Designer.cs
+++ BackTerminal/frmBookNew.Designer.cs
@@ -31,19 +31,21 @@
             this.button3 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.tvCategory = new System.Windows.Forms.TreeView();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBoxISBN = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxPublisher = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBoxQuantity = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxLibrary = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(295, 338);
+            this.button3.Location = new System.Drawing.Point(296, 385);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 21;
@@ -53,7 +55,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(20, 138);
+            this.label4.Location = new System.Drawing.Point(21, 185);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(29, 12);
             this.label4.TabIndex = 18;
@@ -61,17 +63,17 @@
             // 
             // tvCategory
             // 
-            this.tvCategory.Location = new System.Drawing.Point(67, 138);
+            this.tvCategory.Location = new System.Drawing.Point(68, 185);
             this.tvCategory.Name = "tvCategory";
             this.tvCategory.Size = new System.Drawing.Size(204, 180);
             this.tvCategory.TabIndex = 17;
             // 
-            // textBox3
+            // textBoxISBN
             // 
-            this.textBox3.Location = new System.Drawing.Point(68, 71);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 21);
-            this.textBox3.TabIndex = 16;
+            this.textBoxISBN.Location = new System.Drawing.Point(68, 71);
+            this.textBoxISBN.Name = "textBoxISBN";
+            this.textBoxISBN.Size = new System.Drawing.Size(100, 21);
+            this.textBoxISBN.TabIndex = 16;
             // 
             // label3
             // 
@@ -82,12 +84,12 @@
             this.label3.TabIndex = 15;
             this.label3.Text = "ISBN";
             // 
-            // textBox2
+            // textBoxPublisher
             // 
-            this.textBox2.Location = new System.Drawing.Point(67, 43);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 21);
-            this.textBox2.TabIndex = 14;
+            this.textBoxPublisher.Location = new System.Drawing.Point(67, 43);
+            this.textBoxPublisher.Name = "textBoxPublisher";
+            this.textBoxPublisher.Size = new System.Drawing.Size(100, 21);
+            this.textBoxPublisher.TabIndex = 14;
             // 
             // label2
             // 
@@ -98,12 +100,12 @@
             this.label2.TabIndex = 13;
             this.label2.Text = "出版社";
             // 
-            // textBox1
+            // textBoxTitle
             // 
-            this.textBox1.Location = new System.Drawing.Point(67, 16);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 12;
+            this.textBoxTitle.Location = new System.Drawing.Point(67, 16);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(100, 21);
+            this.textBoxTitle.TabIndex = 12;
             // 
             // label1
             // 
@@ -117,34 +119,53 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(22, 105);
+            this.label5.Location = new System.Drawing.Point(21, 108);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(29, 12);
             this.label5.TabIndex = 22;
             this.label5.Text = "数量";
             // 
-            // textBox4
+            // textBoxQuantity
             // 
-            this.textBox4.Location = new System.Drawing.Point(68, 105);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 21);
-            this.textBox4.TabIndex = 23;
+            this.textBoxQuantity.Location = new System.Drawing.Point(68, 105);
+            this.textBoxQuantity.Name = "textBoxQuantity";
+            this.textBoxQuantity.Size = new System.Drawing.Size(100, 21);
+            this.textBoxQuantity.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "图书馆";
+            // 
+            // comboBoxLibrary
+            // 
+            this.comboBoxLibrary.FormattingEnabled = true;
+            this.comboBoxLibrary.Location = new System.Drawing.Point(68, 139);
+            this.comboBoxLibrary.Name = "comboBoxLibrary";
+            this.comboBoxLibrary.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxLibrary.TabIndex = 25;
             // 
             // frmBookNew
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(490, 459);
-            this.Controls.Add(this.textBox4);
+            this.ClientSize = new System.Drawing.Size(490, 418);
+            this.Controls.Add(this.comboBoxLibrary);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxQuantity);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.tvCategory);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBoxISBN);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxPublisher);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxTitle);
             this.Controls.Add(this.label1);
             this.Name = "frmBookNew";
             this.Text = "购买新书";
@@ -158,13 +179,15 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TreeView tvCategory;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBoxISBN;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxPublisher;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxTitle;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBoxQuantity;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBoxLibrary;
     }
 }
